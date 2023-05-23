7c1ba9e3d56c59e99cebcc135b9681e4e6363015
mengru.ji <jmrleo0815@outlook.com>
2023-05-23 22:31:08 +0200

    Add batch identification

--- src/resource_based_analysis.py
+++ src/resource_based_analysis.py
@@ -1,4 +1,13 @@
+from collections import defaultdict
+import pandas as pd
+from flask import render_template, Flask, Blueprint
 
+from src.get_data import get_res_act_relation
+
+resource_based_analysis_app = Blueprint('resource_based_analysis_app', __name__)
+
+
+@resource_based_analysis_app.route('/resource_performance')
 def resource_performance(df):
     """
     Find the most efficient resource and the least efficient resource for each activity
@@ -23,3 +32,57 @@ def resource_performance(df):
 
     return least_efficient, most_efficient
 
+
+@resource_based_analysis_app.route('/batch_identification')
+def batch_identification(df, activities):
+    """
+    Identify all possible batches for al available combination of resources and activities
+    :param df: data frame with case_id, activity, resource, start_at, end_at
+    :param activities: list of all kinds of activities in df
+    :return: batches
+    """
+    res_act_dict = get_res_act_relation(df, activities)
+    batches = defaultdict(dict)
+    group_res_act = df.groupby(["resource", "activity"])
+
+    for a in activities:
+        for r in res_act_dict[a]:
+
+            data_res_act = group_res_act.get_group((r, a))
+            data_res_act["start_at"] = data_res_act["start_at"].round("T")
+            data_res_act["end_at"] = data_res_act["end_at"].round("T")
+            data_res_act = data_res_act.sort_values(by=["start_at", "end_at"])
+
+            sim = []
+            seq = []
+            con = []
+            invalid = []
+
+            data = data_res_act.values
+
+            for row1 in data:
+                for row2 in data:
+                    if sum(row1 != row2) == 0:
+                        continue
+                    if (row1[3] == row2[3]) and (row1[4] == row2[4]):
+                        sim.append(row1)
+                    elif (row1[3] == row2[4]) or (row1[4] == row2[3]):
+                        seq.append(row1)
+                    elif (row1[4] < row2[3]) or (row2[4] < row1[3]):
+                        invalid.append(row1)
+                    else:
+                        con.append(row2)
+            df_sim = pd.DataFrame(sim,
+                                  columns=["case_id", "activity", "resource", "start_at", "end_at"]).drop_duplicates()
+            df_seq = pd.DataFrame(seq,
+                                  columns=["case_id", "activity", "resource", "start_at", "end_at"]).drop_duplicates()
+            df_con = pd.DataFrame(con,
+                                  columns=["case_id", "activity", "resource", "start_at", "end_at"]).drop_duplicates()
+            if not df_sim.empty:
+                batches[(a, r)]["Simultaneous"] = df_sim
+            if not df_seq.empty:
+                batches[(a, r)]["Sequential"] = df_seq
+            if not df_con.empty:
+                batches[(a, r)]["Concurrent"] = df_con
+
+    return batches
