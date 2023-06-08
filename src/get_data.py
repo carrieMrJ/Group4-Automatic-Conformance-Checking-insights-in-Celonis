361a54897844f9faa08c56dbfaa6f86c3669ebf4
mengru.ji <jmrleo0815@outlook.com>
2023-06-08 16:39:26 +0200

    Add functions to get task duration and time distance for further analysis

--- src/get_data.py
+++ src/get_data.py
@@ -44,3 +44,53 @@ def get_target_activity_with_start_end_timestamp(data_mode, table_name, case_col
                PQLColumn(name="end_at", query=f'TARGET("{table_name}"."{timestamp_column}")')]
     res = execute_PQL_query(data_mode, columns)
     return res
+
+
+def get_task_duration_time_distance(data_model, table_name, case_column, activity_column, time_column,
+                                    lifecycle_column):
+    """
+    Get task duration for a certain activity and
+    time distance between different/multiple-tries activities that act as souce and target on DFG
+    :param data_model:
+    :param table_name:
+    :param case_column:
+    :param activity_column:
+    :param time_column:
+    :param lifecycle_column:
+    :return: Two data frame: One for task duration (only for tasks that have start and complete life status);
+    the other one for time distance (between activities act as source and target on DFG)
+    """
+    columns_dur = [PQLColumn(name="case_id", query=f'SOURCE("{table_name}"."{case_column}")'),
+                   PQLColumn(name="start_activity",
+                             query=f'CASE WHEN SOURCE("{table_name}"."{activity_column}") = TARGET("{table_name}"."{activity_column}") THEN SOURCE("{table_name}"."{activity_column}") END'),
+                   PQLColumn(name="end_activity",
+                             query=f'CASE WHEN SOURCE("{table_name}"."{activity_column}") = TARGET("{table_name}"."{activity_column}") THEN TARGET("{table_name}"."{activity_column}") END'),
+                   PQLColumn(name="start_life",
+                             query=f'CASE WHEN SOURCE("{table_name}"."{activity_column}") = TARGET("{table_name}"."{activity_column}") THEN SOURCE("{table_name}"."{lifecycle_column}") END'),
+                   PQLColumn(name="end_life",
+                             query=f'CASE WHEN SOURCE("{table_name}"."{activity_column}") = TARGET("{table_name}"."{activity_column}") THEN TARGET("{table_name}"."{lifecycle_column}") END'),
+                   PQLColumn(name="start_timestamp",
+                             query=f'CASE WHEN SOURCE("{table_name}"."{activity_column}") = TARGET("{table_name}"."{activity_column}") THEN SOURCE("{table_name}"."{time_column}") END'),
+                   PQLColumn(name="end_timestamp",
+                             query=f'CASE WHEN SOURCE("{table_name}"."{activity_column}") = TARGET("{table_name}"."{activity_column}") THEN TARGET("{table_name}"."{time_column}") END'),
+                   PQLColumn(name="task_duration(min)",
+                             query=f'CASE WHEN SOURCE("{table_name}"."{activity_column}") = TARGET("{table_name}"."{activity_column}") THEN minutes_between(SOURCE("{table_name}"."{time_column}"), TARGET("{table_name}"."{time_column}")) ELSE NULL END')
+                   ]
+    res_task_duration = execute_PQL_query(data_model, columns_dur)
+    res_task_duration = res_task_duration.dropna(axis=0, how='any')
+    res_task_duration = res_task_duration[res_task_duration.start_life == "start"]
+
+    columns_dis = [PQLColumn(name="case_id", query=f'SOURCE("{table_name}"."{case_column}")'),
+                   PQLColumn(name="start_activity", query=f'SOURCE("{table_name}"."{activity_column}")'),
+                   PQLColumn(name="end_activity", query=f'TARGET("{table_name}"."{activity_column}")'),
+                   PQLColumn(name="start_life", query=f'SOURCE("{table_name}"."{lifecycle_column}")'),
+                   PQLColumn(name="end_life", query=f'TARGET("{table_name}"."{lifecycle_column}")'),
+                   PQLColumn(name="start_timestamp", query=f'SOURCE("{table_name}"."{time_column}")'),
+                   PQLColumn(name="end_timestamp", query=f'TARGET("{table_name}"."{time_column}")'),
+                   PQLColumn(name="time_distance(min)",
+                             query=f'minutes_between(SOURCE("{table_name}"."{time_column}"), TARGET("{table_name}"."{time_column}"))')
+                   ]
+    res_time_distance = execute_PQL_query(data_model, columns_dis)
+    res_time_distance = res_time_distance[res_time_distance["start_life"] != "start"]
+
+    return res_task_duration, res_time_distance
