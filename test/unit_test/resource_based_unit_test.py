e8ccfdbd69219511d234cd0cc1f26e19bdfafa61
GutenMeorgen <57331249+GutenMeorgen@users.noreply.github.com>
2023-06-13 00:20:15 +0200

    add resource_based_unit_test.py

--- test/unit_test/resource_based_unit_test.py
+++ test/unit_test/resource_based_unit_test.py
@@ -1,8 +1,12 @@
 import unittest
 import pandas as pd
+from unittest.mock import patch
+from io import StringIO
 from src.get_data import get_unique_activity, get_unique_resource
 from src.resource_based_analysis import batch_identification
-
+from src.retrieve_data_for_activities_repeated_by_difference_resources_PQL import get_caseid_activity_lifecycle_resource
+from src.find_high_rework_resources_analysis import find_high_rework_resources
+from src.find_deviations_analysis import find_deviations
 
 class ResourceBasedTestCase(unittest.TestCase):
     def test_unique_activity_resource(self):
@@ -85,6 +89,27 @@ class ResourceBasedTestCase(unittest.TestCase):
                                         "end_at": pd.to_datetime(["2023-01-01 09:20:00", "2023-01-01 10:20:00"])})
         for idx, rows in correct_seq_B_Y.iterrows():
             self.assertIn(rows["case_id"], list(d[key2]["Sequential"]["case_id"]))
+
+    def test_find_deviations(self):
+        example = pd.DataFrame({
+        'case_id': ['case1', 'case1', 'case1', 'case2', 'case2', 'case2', 'case3', 'case3', 'case3', 'case4', 'case4', 'case4'],
+        'activity': ['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'D'],
+        'resource': ['res1', 'res1', 'res1', 'res2', 'res2', 'res2', 'res3', 'res3', 'res3', 'res1', 'res1', 'res1']
+        })
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            find_deviations(example, 1)
+        self.assertIn('Deviation found: Resource res1 performed unusual activity C in case case1', fake_out.getvalue())
+        
+
+    def test_find_high_rework_resources(self):
+        example = pd.DataFrame({
+            "case_id": ['case1', 'case1', 'case1', 'case1', 'case1', 'case1', 'case1', 'case1', 'case1', 'case2', 'case2', 'case2', 'case2', 'case2', 'case2', 'case2', 'case2', 'case2'],
+            "activity": ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+            "resource": ['res1', 'res1', 'res1', 'res1', 'res1', 'res1', 'res2', 'res2', 'res2', 'res3', 'res3', 'res3', 'res3', 'res3', 'res3', 'res4', 'res4', 'res4'],
+            "transition": ['start', 'start', 'start', 'start', 'complete', 'complete', 'complete', 'complete', 'complete', 'start', 'start', 'start', 'start', 'complete', 'complete', 'complete', 'complete', 'complete'],
+        })
+        high_rework_resources = find_high_rework_resources(example, rework_threshold=0, count_threshold=3)
+        self.assertListEqual(high_rework_resources, ['res1', 'res3'])        
 
 
 if __name__ == "__main__":
