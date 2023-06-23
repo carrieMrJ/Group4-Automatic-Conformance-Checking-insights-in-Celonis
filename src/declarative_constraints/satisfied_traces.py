000b36d1a9dd218361c40eebed5fadf9e4853193
pranavvats32 <71003008+pranavvats32@users.noreply.github.com>
2023-06-23 02:05:25 +0200

    updated code and unit test

--- src/declarative_constraints/satisfied_traces.py
+++ src/declarative_constraints/satisfied_traces.py
@@ -4,68 +4,71 @@ from constraint_operations import CONSTRAINT_LIBRARY,regex_dfa
 
 def get_satisfied_constraints(main_trace_list, constraint_list, symbols, constraint_library):
     """
-    Get the list of constraints satisfied by the mainstream traces
-    :param main_trace_list: List of strings representing the mainstream traces
-    :param constraint_list: Dictionary with constraint names as keys and constraint parameters as values
-    :param symbols: List of input symbols
-    :param CONSTRAINT_LIBRARY: Dictionary with constraint names as keys and constraint functions as values
-    :return: List of constraint names satisfied by the mainstream traces
+    Find out the diagnostics
+    :param symbols:
+    :param main_trace_list: list of string
+    :param constraint_list: dictionary key:constraint name value:constraint parameter
+    :return: list of satisfied constraints by the mainstream traces
     """
-    # List of DFAs for constraints
+    # list of dfa for constraints
     dfa4constraints = {}
-
-    for key, value in constraint_list.items():
-        x = constraint_library.get(key)
-
-        if x is None:
-            continue
-
-        if x.__code__.co_argcount == 3:
-            for item in value:
-                regex = x(symbols, item[0], item[1])
-                dfa4constraints[f"{key}:{item}"] = regex_dfa(regex)
-        else:
-            for item in value:
-                regex = x(symbols, item)
-                dfa4constraints[f"{key}:{item}"] = regex_dfa(regex)
-
-    satisfied_constraints = []
-
+    # list of valid constraints
+    valid_constraints = defaultdict(list)
+    try:
+        for key, value in constraint_list.items():
+            template = constraint_library[key]
+            if template.__code__.co_argcount == 3:
+                for item in value:
+                    regex = template(symbols, item[0], item[1])
+                    dfa4constraints[f"{key}:{item}"] = regex_dfa(regex)
+            else:
+                for item in value:
+                    regex = template(symbols, item)
+                    dfa4constraints[f"{key}:{item}"] = regex_dfa(regex)
+    except KeyError:
+        pass
     for trace in main_trace_list:
         for key, dfa in dfa4constraints.items():
             constraint_name = key.split("(")[0]
-            if dfa.accepts_input(trace):
-                satisfied_constraints.append(constraint_name)
-                break
-
+            flag = dfa.accepts_input(trace)
+            if flag:
+                valid_constraints[constraint_name].append(trace)
+    satisfied_constraints=list(valid_constraints.keys())
     return satisfied_constraints
 
+import re
 
-def find_anomalies(new_observation_traces, satisfied_constraints, symbols, constraint_library):
+def find_anomalies(new_observation_traces, satisfied_constraints, symbols, CONSTRAINT_LIBRARY):
     """
-    Find anomalies in the observation traces based on the satisfied constraints
-    :param new_observation_traces: List of strings representing the observation traces
-    :param satisfied_constraints: List of constraints satisfied by the mainstream traces
-    :param symbols: List of input symbols
-    :param constraint_library: Dictionary with constraint names as keys and constraint functions as values
-    :return: List of anomaly traces (new observation traces that dont satisfy the constraints which were satisfied by mainstream cases)
+    Find anomalies in new observation traces based on satisfied constraints
+    :param new_observation_traces: list of strings (new observation traces)
+    :param satisfied_constraints: list of satisfied constraints
+    :param symbols: list of symbols
+    :param CONSTRAINT_LIBRARY: dictionary of constraint templates
+    :return: list of anomalies (traces that do not satisfy the constraints)
     """
-    anomaly_traces = []
+    anomalies = []
 
     for trace in new_observation_traces:
-        for constraint_name in satisfied_constraints:
-            template = constraint_library.get(constraint_name)
-            if template is not None:
-                if template.__code__.co_argcount == 3:
-                    for symbol1 in symbols:
-                        for symbol2 in symbols:
-                            if symbol1 != symbol2 and not template(symbols, symbol1, symbol2).accepts_input(trace):
-                                anomaly_traces.append(trace)
-                                break
-                else:
-                    for symbol in symbols:
-                        if not template(symbols, symbol).accepts_input(trace):
-                            anomaly_traces.append(trace)
-                            break
+        trace_satisfies_constraints = False
 
-    return anomaly_traces
+        for constraint in satisfied_constraints:
+            constraint_name, *constraint_params = constraint.split(":")
+            template = CONSTRAINT_LIBRARY[constraint_name]
+
+            if template.__code__.co_argcount == 3:
+                for params in constraint_params:
+                    regex = template(symbols, *params)
+                    if re.match(regex, trace):
+                        trace_satisfies_constraints = True
+                        break
+            else:
+                regex = template(symbols, *constraint_params)
+                if re.match(regex, trace):
+                    trace_satisfies_constraints = True
+                    break
+
+        if not trace_satisfies_constraints:
+            anomalies.append(trace)
+
+    return anomalies
