49b6477e1ce9f9ce4ba1125d3debbb2c82cc6019
mengruji <jmrleo0815@outlook.com>
2023-07-01 12:43:58 +0200

    update the unit tests for constraint templates

--- test/unit_test/declarative_constraints_templates_unit_test.py
+++ test/unit_test/declarative_constraints_templates_unit_test.py
@@ -11,93 +11,93 @@ class DeclarativeTestMethods(unittest.TestCase):
     # unit tests for constraint templates
     def test_startWith(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(startWith(input_symbols, 'a'), 'a(a|b|c|d)*')
-        self.assertEqual(startWith(input_symbols, 'c'), 'c(a|b|c|d)*')
+        self.assertEqual(startWith(input_symbols, 'a'), '^a')
+        self.assertEqual(startWith(input_symbols, 'c'), '^c')
 
     def test_endWith(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(endWith(input_symbols, 'd'), '(a|b|c|d)*d')
-        self.assertEqual(endWith(input_symbols, 'b'), '(a|b|c|d)*b')
+        self.assertEqual(endWith(input_symbols, 'd'), 'd$')
+        self.assertEqual(endWith(input_symbols, 'b'), 'b$')
 
     def test_atMostOnce(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(atMostOnce(input_symbols, 'a'), '(b|c|d)*a?(b|c|d)*')
-        self.assertEqual(atMostOnce(input_symbols, 'c'), '(a|b|d)*c?(a|b|d)*')
+        self.assertEqual(atMostOnce(input_symbols, 'a'), '^[^a]*a?[^a]*$')
+        self.assertEqual(atMostOnce(input_symbols, 'c'), '^[^c]*c?[^c]*$')
 
     def test_atLeastOnce(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(atLeastOnce(input_symbols, 'a'), '(a|b|c|d)*a(a|b|c|d)*')
-        self.assertEqual(atLeastOnce(input_symbols, 'c'), '(a|b|c|d)*c(a|b|c|d)*')
+        self.assertEqual(atLeastOnce(input_symbols, 'a'), '^.*a.*$')
+        self.assertEqual(atLeastOnce(input_symbols, 'c'), '^.*c.*$')
 
     def test_never(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(never(input_symbols, 'a'), '(b|c|d)*')
-        self.assertEqual(never(input_symbols, 'c'), '(a|b|d)*')
+        self.assertEqual(never(input_symbols, 'a'), '^(?!.*a).*')
+        self.assertEqual(never(input_symbols, 'c'), '^(?!.*c).*')
 
     def test_precedence(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(precedence(input_symbols, 'a', 'b'), '(c|d)*(a(a|b|c|d)*)?')
-        self.assertEqual(precedence(input_symbols, 'c', 'd'), '(a|b)*(c(a|b|c|d)*)?')
+        self.assertEqual(precedence(input_symbols, 'a', 'b'), '[^b]*a.*b.*')
+        self.assertEqual(precedence(input_symbols, 'c', 'd'), '[^d]*c.*d.*')
 
     def test_alternate_precedence(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(alternate_precedence(input_symbols, 'a', 'b'), '(c|d)*((a(a|c|d)*b(c|d)*)|(a(c|d)*))*')
-        self.assertEqual(alternate_precedence(input_symbols, 'c', 'd'), '(a|b)*((c(a|b|c)*d(a|b)*)|(c(a|b)*))*')
+        self.assertEqual(alternate_precedence(input_symbols, 'a', 'b'), '[^b]*(a(?!b)*)*')
+        self.assertEqual(alternate_precedence(input_symbols, 'c', 'd'), '[^d]*(c(?!d)*)*')
 
     def test_chain_precedence(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(chain_precedence(input_symbols, 'a', 'b'), '(c|d)*((a+b(c|d)*)|(a(c|d)*))*')
-        self.assertEqual(chain_precedence(input_symbols, 'c', 'd'), '(a|b)*((c+d(a|b)*)|(c(a|b)*))*')
+        self.assertEqual(chain_precedence(input_symbols, 'a', 'b'), '[^b]*(ab)*[^b]*')
+        self.assertEqual(chain_precedence(input_symbols, 'c', 'd'), '[^d]*(cd)*[^d]*')
 
     def test_responded_existence(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(responded_existence(input_symbols, 'a', 'b'), '(b|c|d)*(a(a|c|d)*b(a|b|c|d)*)?')
-        self.assertEqual(responded_existence(input_symbols, 'c', 'd'), '(a|b|d)*(c(a|b|c)*d(a|b|c|d)*)?')
+        self.assertEqual(responded_existence(input_symbols, 'a', 'b'), '[^a]*(a[^b]*b.*)?')
+        self.assertEqual(responded_existence(input_symbols, 'c', 'd'), '[^c]*(c[^d]*d.*)?')
 
     def test_response(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(response(input_symbols, 'a', 'b'), '(c|d)*((b(b|c|d)*)|(a(a|c|d)*b(b|c|d)*))?')
-        self.assertEqual(response(input_symbols, 'c', 'd'), '(a|b)*((d(a|b|d)*)|(c(a|b|c)*d(a|b|d)*))?')
+        self.assertEqual(response(input_symbols, 'a', 'b'), '[^a]*(ab)*[^a]*')
+        self.assertEqual(response(input_symbols, 'c', 'd'), '[^c]*(cd)*[^c]*')
 
     def test_alternate_response(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(alternate_response(input_symbols, 'a', 'b'), '(b|c|d)*((a(c|d)*b(b|c|d)*)+)?')
-        self.assertEqual(alternate_response(input_symbols, 'c', 'd'), '(a|b|d)*((c(a|b)*d(a|b|d)*)+)?')
+        self.assertEqual(alternate_response(input_symbols, 'a', 'b'), '[^a]*((a[^ab]*b[^a]*)+)?')
+        self.assertEqual(alternate_response(input_symbols, 'c', 'd'), '[^c]*((c[^cd]*d[^c]*)+)?')
 
     def test_chain_response(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(chain_response(input_symbols, 'a', 'b'), '(b|c|d)*((ab(b|c|d)*)+)?')
-        self.assertEqual(chain_response(input_symbols, 'c', 'd'), '(a|b|d)*((cd(a|b|d)*)+)?')
+        self.assertEqual(chain_response(input_symbols, 'a', 'b'), '[^a]*((ab[^a]*)+)?')
+        self.assertEqual(chain_response(input_symbols, 'c', 'd'), '[^c]*((cd[^c]*)+)?')
 
     def test_succession(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(succession(input_symbols, 'a', 'b'), '(b|c|d)*(a(a|c|d)*b(b|c|d)*)*')
-        self.assertEqual(succession(input_symbols, 'c', 'd'), '(a|b|d)*(c(a|b|c)*d(a|b|d)*)*')
+        self.assertEqual(succession(input_symbols, 'a', 'b'), '[^a]*(a[^b]*b[^a]*)*')
+        self.assertEqual(succession(input_symbols, 'c', 'd'), '[^c]*(c[^d]*d[^c]*)*')
 
     def test_alternate_succession(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(alternate_succession(input_symbols, 'a', 'b'), '(c|d)*(a(c|d)*b(c|d)*)*')
-        self.assertEqual(alternate_succession(input_symbols, 'c', 'd'), '(a|b)*(c(a|b)*d(a|b)*)*')
+        self.assertEqual(alternate_succession(input_symbols, 'a', 'b'), '[^ab]*(a([^ab])*b)*([^ab])*')
+        self.assertEqual(alternate_succession(input_symbols, 'c', 'd'), '[^cd]*(c([^cd])*d)*([^cd])*')
 
     def test_chain_succession(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(chain_succession(input_symbols, 'a', 'b'), '(b|c|d)*(ab(c|d)*)*')
-        self.assertEqual(chain_succession(input_symbols, 'c', 'd'), '(a|b|d)*(cd(a|b)*)*')
+        self.assertEqual(chain_succession(input_symbols, 'a', 'b'), '[^a]*(ab[^ab]*)*')
+        self.assertEqual(chain_succession(input_symbols, 'c', 'd'), '[^c]*(cd[^cd]*)*')
 
     def test_not_coexistence(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(not_coexistence(input_symbols, 'a', 'b'), '(c|d)*(a(a|c|d)*|b(b|c|d)*)?')
-        self.assertEqual(not_coexistence(input_symbols, 'c', 'd'), '(a|b)*(c(a|b|c)*|d(a|b|d)*)?')
+        self.assertEqual(not_coexistence(input_symbols, 'a', 'b'), '[^ab]*(a[^b]*|b[^a]*)?')
+        self.assertEqual(not_coexistence(input_symbols, 'c', 'd'), '[^cd]*(c[^d]*|d[^c]*)?')
 
     def test_not_succession(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(not_succession(input_symbols, 'a', 'b'), '(b|c|d)*a(a|c|d)*')
-        self.assertEqual(not_succession(input_symbols, 'c', 'd'), '(a|b|d)*c(a|b|c)*')
+        self.assertEqual(not_succession(input_symbols, 'a', 'b'), '[^a]*a[^b]*')
+        self.assertEqual(not_succession(input_symbols, 'c', 'd'), '[^c]*c[^d]*')
 
     def test_not_chain_succession(self):
         input_symbols = ['a', 'b', 'c', 'd']
-        self.assertEqual(not_chain_succession(input_symbols, 'a', 'b'), '(b|c|d)*(a+(c|d)(b|c|d)*)*(a+)?')
-        self.assertEqual(not_chain_succession(input_symbols, 'c', 'd'), '(a|b|d)*(c+(a|b)(a|b|d)*)*(c+)?')
+        self.assertEqual(not_chain_succession(input_symbols, 'a', 'b'), '[^a]*(a+[^ab][^a]*)*(a+)?')
+        self.assertEqual(not_chain_succession(input_symbols, 'c', 'd'), '[^c]*(c+[^cd][^c]*)*(c+)?')
 
 
 if __name__ == '__main__':
