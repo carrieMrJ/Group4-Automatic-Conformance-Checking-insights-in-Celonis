08e57654959d32bc769be2ab7a94616bf4402512
mengruji <jmrleo0815@outlook.com>
2023-07-02 01:36:46 +0200

    delete reverse_mapping

--- src/get_data.py
+++ src/get_data.py
@@ -1,9 +1,6 @@
 import string
-
 from pycelonis.pql import PQLColumn
-
-from src.celonis_data_integration import execute_PQL_query, get_connection, check_invalid_table_in_celonis, \
-    get_celonis_info
+from src.celonis_data_integration import execute_PQL_query
 
 
 def get_execution_time_per_res_per_act(data_mode, table_name, case_column, activity_column, resource_column,
@@ -48,60 +45,19 @@ def get_target_activity_with_start_end_timestamp(data_mode, table_name, case_col
     return res
 
 
-def mapping_traces(data_model, table_name, activity_column, trace_list):
-    def map_activities(data_model_inside, table_name_inside, activity_column_inside):
-        col = [PQLColumn(name="activity", query=f'"{table_name_inside}"."{activity_column_inside}"')]
-        activities = execute_PQL_query(data_model_inside, col, distinct=True)["activity"]
-        num_act = len(activities)
-        mapping_inside = dict()
-        set1 = string.ascii_letters[:]
-        set1 += '0123456789'
-        if num_act <= len(set1):
-            for i in range(num_act):
-                mapping_inside[activities[i]] = set1[i]
-        else:
-            return f'Too many activities, need to provide more candidates for mapping!'
-        return mapping_inside, activities
-
-    mapping, act_l = map_activities(data_model, table_name, activity_column)
-    print(mapping)
-    # col2 = [PQLColumn(name="activity_trace", query=f'VARIANT("{table_name}"."{activity_column}")')]
-    # main_trace_list = execute_PQL_query(data_model, col2, distinct=True)["activity_trace"]
-    traces = []
-    for t in trace_list:
-        tmp = t.split(", ")
-        trace = ""
-        for act in tmp:
-            trace += mapping[act]
-        traces.append(trace)
-    return traces, mapping, list(mapping.values())
-
-
-def reverse_mapping(encoded_traces, mapping, values):
-    # Create a reverse mapping from the mapping dictionary
-    reverse = {v: k for k, v in mapping.items()}
-
-    # Check if all values are in reverse mapping keys
-    if set(values).issubset(set(reverse.keys())):
-        # Initialize an empty list for the original traces
-        original_traces = []
-
-        # Iterate over each encoded trace
-        for trace in encoded_traces:
-            # Initialize an empty string to store the original trace
-            original_trace = ""
-
-            # Iterate over each character (activity) in the trace
-            for act in trace:
-                # Append the original activity to the original_trace string, followed by ", "
-                original_trace += reverse[act] + ", "
-
-            # Append the original trace to the list of original traces
-            # Remove the trailing comma and space
-            original_traces.append(original_trace.rstrip(", "))
-
-        # Return the list of original traces
-        return original_traces
-
+def encode_activities(data_model, table_name, activity_column):
+    col = [PQLColumn(name="activity", query=f'"{table_name}"."{activity_column}"')]
+    activities = execute_PQL_query(data_model, col, distinct=True)["activity"]
+    num_act = len(activities)
+    mapping = dict()
+    set1 = string.ascii_letters[:]
+    set1 += '0123456789'
+    if num_act <= len(set1):
+        for i in range(num_act):
+            mapping[activities[i]] = set1[i]
+        reverse_mapping = {value: key for key, value in mapping.items()}
     else:
-        return "The provided mapping values do not match the mapping dictionary."
+        print(f'Too many activities, need to provide more candidates for mapping!')
+        return None, None, None
+    return mapping, reverse_mapping, activities
+
