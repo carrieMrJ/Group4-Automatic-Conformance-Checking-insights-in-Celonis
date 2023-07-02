b0b5a512525f1bde2805bdb5b2252562d2650682
Zhenyu Hu <57331249+GutenMeorgen@users.noreply.github.com>
2023-07-02 14:00:55 +0200

    Update get_data.py

--- src/data_integration/get_data.py
+++ src/data_integration/get_data.py
@@ -18,6 +18,16 @@ def get_execution_time_per_res_per_act(data_mode, table_name, case_column, activ
     res = execute_PQL_query(data_mode, columns)
     return res
 
+def get_caseid_activity_lifecycle_resource(data_model, table_name, case_column, activity_column, resource_column,
+                                       lifecycle_column):
+    columns = [PQLColumn(name="case_id", query=f'SOURCE("{table_name}"."{case_column}")'),
+               PQLColumn(name="activity", query=f'SOURCE("{table_name}"."{activity_column}")'),
+               PQLColumn(name="transition", query=f'SOURCE("{table_name}"."{lifecycle_column}")'),
+               PQLColumn(name="resource", query=f'SOURCE("{table_name}"."{resource_column}")')
+              ]
+    res = execute_PQL_query(data_model, columns)
+    return res
+
 
 def get_unique_activity(df, act_column_name):
     return df[act_column_name].unique()
